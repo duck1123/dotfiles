2a780ec2ea12b1bb33fd948e0bf018a0671fe161
Daniel E. Renfer <daniel.renfer@helloworld.com>
2015-10-23 17:18:19 -0400

    add hideshow support

--- emacs/custom.el
+++ emacs/custom.el
@@ -9,7 +9,9 @@
                          ))
 
 (prelude-require-packages
- '(ack-and-a-half auth-password-store bts bts-github ctags helm-ack helm-github-stars log4j-mode mingus nyan-mode org-gcal))
+ '(ack-and-a-half auth-password-store bts bts-github ctags helm-ack
+                  helm-github-stars hideshow-org log4j-mode mingus nyan-mode
+                  org-gcal))
 
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
 (global-set-key (kbd "<mouse-6>") 'next-buffer)
@@ -51,6 +53,8 @@
 (set-time-zone-rule "GMT")
 
 (load-file "~/.dotfiles/emacs/private.el.gpg")
+
+(require 'hideshow-org)
 
 (add-hook 'php-mode-hook
           (lambda ()
