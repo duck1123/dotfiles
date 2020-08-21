20cccbf13a657284f5808ca031b8117a3988f9e6
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-21 09:41:15 -0400

    Update melpa directory

--- emacs/custom.el
+++ emacs/custom.el
@@ -10,7 +10,7 @@
                          ;; ("elpa" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ))
 
