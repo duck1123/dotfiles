d1a50bb034704bf26fbb5c16ba92657804b44611
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-05-15 11:18:10 -0400

    clean up home nix

--- nix/home.nix
+++ nix/home.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 
 let
+  name = "Duck Nebuchadnezzar";
   username = "duck";
+  email = "duck@kronkltd.net";
 in
 {
   # Let Home Manager install and manage itself.
@@ -16,10 +18,14 @@ in
     homeDirectory = "/home/${username}";
 
     packages = with pkgs; [
+      bat
       curl
       fish
       emacs
       helm
+      hstr
+      keepassxc
+      kubectl
       nixfmt
       tdesktop
     ];
@@ -43,17 +49,91 @@ in
 
   programs.git = {
     enable = true;
-    userName = "Duck Nebuchadnezzar";
-    userEmail = "duck@kronkltd.net";
+    userName = "${name}";
+    userEmail = "${email}";
     # signing = {
     #   signByDefault = true;
     #   key = "";
     # };
   };
 
+  programs.ncmpcpp = {
+    enable = true;
+    settings = {
+      # Playlist
+      now_playing_prefix = "$b";
+      now_playing_suffix = "$/b";
+      playlist_display_mode = "columns (classic/columns)";
+      autocenter_mode = "yes";
+      centered_cursor = "yes";
+      visualizer_fifo_path = "/tmp/mpd.fifo";
+      visualizer_output_name = "my_fifo";
+      visualizer_sync_interval = "30";
+      visualizer_in_stereo = "yes";
+      # visualizer_type = "wave" (spectrum/wave)
+      visualizer_type = "spectrum (spectrum/wave)";
+      visualizer_color = "green";
+      visualizer_look = "∙▋";
+      # progressbar_look = "━■"
+      progressbar_look = "▀▀ ";
+      progressbar_color = "black";
+      progressbar_elapsed_color = "blue";
+      # Bars
+      song_list_format = " $1%a $5//$8 %t";
+      # song_list_format = "$1• $8%t $1by $2%a$2 $R$1%l";
+      # song_list_format = "$8 %t $R$8";
+      song_status_format = " $2%a $4⟫$3⟫ $8%t $4⟫$3⟫ $5%b ";
+      # song_status_format = "%t » %a »{ %b » }%y";
+      # progressbar_look = "|] ";
+      titles_visibility = "no";
+      song_columns_list_format = "(6f)[default]{l} (40)[default]{t|f} (25)[default]{a} (30)[default]{b}";
+      # Browser
+      browser_playlist_prefix = "$2plist »$9 ";
+      browser_display_mode = "columns (classic/columns)";
+      mouse_support = "yes";
+      header_visibility = "no";
+      statusbar_visibility = "yes";
+      enable_window_title = "no";
+      # Colors
+      discard_colors_if_item_is_selected = "yes";
+      header_window_color = "default";
+      volume_color = "default";
+      state_line_color = "default";
+      state_flags_color = "default";
+      main_window_color = "default";
+      color1 = "default";
+      color2 = "default";
+      main_window_highlight_color = "white";
+      statusbar_color = "default";
+      active_column_color = "default";
+      # Others
+      song_window_title_format = "{%a - }{%t}{ - %b{ Disc %d}}|{%f}";
+      search_engine_display_mode = "columns (classic/columns)";
+      follow_now_playing_lyrics = "yes";
+      display_screens_numbers_on_start = "no";
+      clock_display_seconds = "yes";
+      # execute_on_song_change = "twmnc -c \"$(ncmpcpp --now-playing %a' - '%t)\";
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+  };
+
+  programs.vim = {
+    enable = true;
+    extraConfig = ''
+        syntax on
+        " Wrap gitcommit file types at the appropriate length
+        filetype indent plugin on
+    '';
+  };
+
   programs.zsh = {
     enable = true;
+    enableAutosuggestions = true;
     defaultKeymap = "emacs";
+
     history = {
       extended = true;
     };
@@ -83,10 +163,6 @@ in
     };
 
     plugins = [
-      # {
-      #   name = "bb-task-completion";
-      #   src = "~/projects/bb-task-completion";
-      # }
       {
         name = "bb-task-completion";
         src = pkgs.fetchFromGitHub {
@@ -102,52 +178,29 @@ in
         if [ -e /home/duck/.nix-profile/etc/profile.d/nix.sh ]; then
             . /home/duck/.nix-profile/etc/profile.d/nix.sh;
         fi # added by Nix installer
-        export PATH="~/.yarn/bin:~/.config/yarn/global/node_modules/.bin:$PATH"
+        export PATH="~/.yarn/bin:$PATH"
+        export PATH="~/.config/yarn/global/node_modules/.bin:$PATH"
         export PATH="~/.huber/bin:$PATH"
 
-        # export HSTR_CONFIG=hicolor       # get more colors
         bindkey -s "\C-r" "\C-a hstr -- \C-j"     # bind hstr to Ctrl-r (for Vi mode check doc)
         source <(doctl completion zsh)
         source <(k3d completion zsh)
     '';
 
-    # initExtraFirst = ''
-    #     echo a
-    #     echo b
-    # '';
-
     localVariables = {
       PROJECT_PATHS = [ ~/projects ];
-      # fpath = [
-      #   \$fpath
-      #   "~/projects/bb-task-completion/"
-      # ];
+    };
+
+    sessionVariables = {
+      HSTR_CONFIG = "hicolor";
     };
 
     shellAliases = {
-      "reload!" = ". ~/.zshrc";
-      psgrep = "ps -ef | grep -v grep | grep ";
+      cat = "bat";
       dkcp = "docker-compose";
       hh = "hstr";
+      psgrep = "ps -ef | grep -v grep | grep ";
+      "reload!" = "home-manager switch && . ~/.zshrc";
     };
-
-    loginExtra = ''
-      echo "Running login extra"
-    '';
-
-    enableAutosuggestions = true;
-#     profileExtra = ''
-# emulate sh
-# if [ -f /etc/profile ] && [ ! -v __ETC_PROFILE_DONE ]; then
-#   . /etc/profile
-# fi
-# if [ -f ~/.profile ]; then
-#   . ~/.profile
-# fi
-# emulate zsh
-# if [[ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]]; then
-#   source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-# fi
-# '';
   };
 }
