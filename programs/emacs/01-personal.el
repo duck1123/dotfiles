9160524cebf5776a4c7572781aa68f6b6245a156
Duck Nebuchadnezzar <duck@kronkltd.net>
2022-04-09 18:09:36 -0400

    update prelude modules

--- programs/emacs/01-personal.el
+++ programs/emacs/01-personal.el
@@ -3,38 +3,20 @@
 
 ;;; Code:
 (setq prelude-guru nil)
+(setq load-package-verbose t)
 
 ;; (setq package-archives
 ;;       '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;         ("melpa" . "https://melpa.org/packages/")))
 
 (prelude-require-packages
- '(auto-complete
-   clojure-mode
-   cider
-   company-flow
-   crappy-jsp-mode
+ '(crappy-jsp-mode
    editorconfig
-   flycheck-clj-kondo
-   flycheck-flow
    helm-projectile
-   hideshow-org
    magit
    paredit
    prettier-js))
 
-(require 'prelude-helm) ;; Interface for narrowing and search
-(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
-(require 'prelude-company)
-(require 'prelude-clojure)
-(require 'prelude-css)
-(require 'prelude-js)
-(require 'prelude-shell)
-(require 'prelude-web) ;; Emacs mode for web templates
-(require 'prelude-xml)
-(require 'prelude-yaml)
-
-(global-set-key (kbd "S-C-g")       'helm-github-stars)
 (global-set-key (kbd "C-x C-k")     'kill-this-buffer)
 
 (setq create-lockfiles nil)
@@ -44,11 +26,18 @@
 (require 'ag-and-a-half)
 (defalias 'ag 'ag-and-a-half)
 
+(use-package auto-complete
+  :ensure t)
+
 (use-package earthfile-mode :ensure t)
-(use-package kubernetes :ensure t)
-(use-package kubernetes-helm :ensure t)
-(use-package helm :ensure t)
-(use-package helm-github-stars :ensure t)
+;; (use-package kubernetes :ensure t)
+;; (use-package kubernetes-helm :ensure t)
+;; (use-package helm :ensure t)
+
+(use-package helm-github-stars
+  :ensure t
+  :commands (helm-github-stars)
+  :bind (("S-C-g" . helm-github-stars)))
 
 (use-package nix-mode
   :ensure t)
@@ -63,15 +52,29 @@
   :init
   (rainbow-delimiters-mode-enable))
 
-(use-package typescript-mode :ensure t)
+(use-package typescript-mode
+  :ensure t)
 
-(use-package scss-mode :ensure t)
+(use-package scss-mode
+  :ensure t)
 (use-package docker :ensure t)
+
 (use-package gradle-mode :ensure t)
-(use-package flycheck-gradle :ensure t)
+(use-package flycheck-gradle
+  :ensure t)
 (use-package lsp-ui :ensure t)
-(use-package company :ensure t)
-(use-package go-mode :ensure t)
+
+(use-package company
+  :ensure t)
+
+(use-package company-flow
+  :ensure t)
+
+(use-package cider
+  :ensure t)
+
+(use-package go-mode
+  :ensure t)
 
 (use-package org-roam
   :ensure t
@@ -80,7 +83,7 @@
   (org-roam-directory "~/Nextcloud/org-roam")
   (org-roam-capture-templates
    `(("d" "default" plain "%?"
-      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}" "${title}\n#+date: %U\n")
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
       :unnarrowed t)))
 
   :bind
@@ -89,13 +92,15 @@
    ("C-c n i" . org-roam-node-insert))
 
   :config
-  (org-roam-setup))
+  (org-roam-setup)
+  (org-roam-db-autosync-mode)
+  (setq org-roam-dailies-capture-templates
+        `(("d" "default" entry "* %?\n:PROPERTIES:\n:CREATED: [%U]\n:END:"
+           :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n")))))
 
-(setq org-roam-dailies-capture-templates
-      `(("d" "default" entry "* %<%I:%M %p>: %?"
-         :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
 
-(require 'magit-lfs)
+(use-package magit-lfs
+  :ensure t)
 
 ;; (add-hook 'java-mode-hook
 ;;           (lambda ()
@@ -105,7 +110,6 @@
 
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(require 'flycheck-clj-kondo)
 
 (dolist (checker '(clj-kondo-clj clj-kondo-cljs clj-kondo-cljc clj-kondo-edn))
   (setq flycheck-checkers (cons checker (delq checker flycheck-checkers))))
@@ -113,16 +117,22 @@
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . crappy-jsp-mode))
 (add-to-list 'auto-mode-alist '("\\.flow\\'" . js2-mode))
 
-(require 'hideshow-org)
-(require 'flycheck-flow)
+(use-package flycheck-clj-kondo
+  :ensure t)
+
+(use-package flycheck-flow
+  :ensure t)
+
+(use-package hideshow-org
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook (lambda () (hs-org/minor-mode 1)))
+  (add-hook 'php-mode-hook     (lambda () (hs-org/minor-mode 1)))
+  (add-hook 'js2-mode-hook     (lambda () (hs-org/minor-mode 1)))
+  (add-hook 'nix-mode-hook     (lambda () (hs-org/minor-mode 1))))
 
 ;; (add-hook 'clojure-mode-hook #'subword-mode)
 ;; (add-hook 'clojure-mode-hook #'paredit-mode)
-
-(add-hook 'clojure-mode-hook (lambda () (hs-org/minor-mode 1)))
-(add-hook 'php-mode-hook     (lambda () (hs-org/minor-mode 1)))
-(add-hook 'js2-mode-hook     (lambda () (hs-org/minor-mode 1)))
-(add-hook 'nix-mode-hook     (lambda () (hs-org/minor-mode 1)))
 
 ;; (add-hook 'c-mode-common-hook (lambda () (c-set-offset 'case-label '+)))
 ;; (add-hook 'js2-mode-hook      (lambda () (c-set-offset 'case-label '+)))
@@ -135,12 +145,17 @@
 ;; (setq tab-width 2)
 ;; (setq default-tab-width 2)
 
-(require 'kubernetes)
-(require 'kubernetes-helm)
+;; (require 'kubernetes)
+;; (require 'kubernetes-helm)
 
 ;; see https://github.com/nextjournal/clerk
-(require 'clojure-mode)
-(require 'cider-format)
+;; (require 'clojure-mode)
+;; (require 'cider-format)
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (define-key clojure-mode-map (kbd "<M-return>") 'clerk-show))
 
 (defun clerk-show ()
   (interactive)
@@ -152,7 +167,5 @@
       (cider-interactive-eval
        (concat "(nextjournal.clerk/show! \"" filename "\")")))))
 
-(define-key clojure-mode-map (kbd "<M-return>") 'clerk-show)
 
 ;; enable autosync
-(org-roam-db-autosync-mode)
--- programs/emacs/default.nix
+++ programs/emacs/default.nix
@@ -4,7 +4,7 @@
   # };
 
   home.file.".emacs.d/personal/01-personal.el".source = ./01-personal.el;
-
+  home.file.".emacs.d/personal/prelude-modules.el".source = ./prelude-modules.el;
 
   # home.file.".emacs/init.el".text = ''
   #   (load "default.el")
