16472dfdd5e67bc792cf64a584b3c53fa81dbc0d
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-05-27 16:11:18 -0400

    don't auto complete files

--- home.nix
+++ home.nix
@@ -196,10 +196,11 @@ in {
       _bb_tasks() {
         local matches=(`bb tasks |tail -n +3 |cut -f1 -d ' '`)
         compadd -a matches
-        _files # autocomplete filenames as well
       }
       compdef _bb_tasks bb
     '';
+
+    # _files # autocomplete filenames as well
 
     localVariables = { PROJECT_PATHS = [ ~/projects ]; };
 
