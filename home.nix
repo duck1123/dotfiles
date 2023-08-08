e0197c48f41cee7fe624564fbc04c3900325555f
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-08-08 09:34:56 -0400

    remove flex-mpv

--- home.nix
+++ home.nix
@@ -57,7 +57,7 @@ in {
       openjdk17
       plex-media-player
       # plex-media-server
-      plex-mpv-shim
+      # plex-mpv-shim
       # plexmediaserver
       # plexmediaserver
       # nextcloud-24.0.3
