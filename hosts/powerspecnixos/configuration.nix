2b5c566d2ef05f8b9cab7ed2ab54345ba36d7ef0
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-02-25 11:18:41 -0500

    update configuration

--- hosts/powerspecnixos/configuration.nix
+++ hosts/powerspecnixos/configuration.nix
@@ -6,36 +6,59 @@
   imports = [ ./hardware-configuration.nix ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    binfmt.registrations.appimage = {
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      magicOrExtension = "\\x7fELF....AI\\x02";
+      mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+      offset = 0;
+      recognitionType = "magic";
+      wrapInterpreterInShell = false;
+    };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [ git zsh ];
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  hardware.pulseaudio.enable = false;
 
-  # Set your time zone.
-  time.timeZone = "America/Detroit";
+  i18n = {
+    # Select internationalisation properties.
+    defaultLocale = "en_US.UTF-8";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+
+    # Enable networking
+    networkmanager.enable = true;
+
+    # Enables wireless support via wpa_supplicant.
+    # wireless.enable = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   programs.gnupg.agent = {
     enable = true;
@@ -44,52 +67,61 @@
 
   programs.steam.enable = true;
 
-  services.emacs.enable = true;
+  security.rtkit.enable = true;
 
-  services.flatpak.enable = true;
+  services = {
+    emacs.enable = true;
+    flatpak.enable = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    # Enable the OpenSSH daemon.
+    # openssh.enable = true;
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    # jack.enable = true;
+    pipewire = {
+      enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    # media-session.enable = true;
-  };
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+      pulse.enable = true;
+    };
 
-  # Configure keymap in X11
-  services.xserver = {
-    # Enable the X11 windowing system.
-    enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
+    # Configure keymap in X11
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
 
-    layout = "us";
+      # Enable the GNOME Desktop Environment.
+      desktopManager.gnome.enable = true;
+      displayManager.gdm.enable = true;
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # libinput.enable = true;
+      # Enable touchpad support (enabled default in most desktopManager).
+      # libinput.enable = true;
 
-    xkbVariant = "";
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
   };
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
 
-  virtualisation.docker.enable = true;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  # Set your time zone.
+  time.timeZone = "America/Detroit";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.duck = {
@@ -99,25 +131,5 @@
     packages = with pkgs; [ appimage-run firefox ];
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  virtualisation.docker.enable = true;
 }
