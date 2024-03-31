c1747df91871c6d185a9686f2998105e76ac3eb9
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-03-31 13:12:21 -0400

    prefer dark

--- machines/powerspecnix/home-for-flake.nix
+++ machines/powerspecnix/home-for-flake.nix
@@ -239,6 +239,8 @@ in {
   };
 
   dconf.settings = {
+    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
     "org/gnome/desktop/wm/preferences".button-layout =
       ":minimize,maximize,close";
 
