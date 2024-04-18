1f3c8892f16cb990c12848baaa128d5c4dd94dac
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-04-18 09:49:24 -0400

    cleanup

--- flake.nix
+++ flake.nix
@@ -7,26 +7,29 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
 
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Utilities for building our flake
+    flake-utils.url = "github:numtide/flake-utils";
+
+    home = {
+      url = "github:nix-community/home-manager/release-22.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       # Use system packages list where available
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Track channels with commits tested and built by hydra
-    # nixos.url = "github:nixos/nixpkgs/nixos-22.05";
-
-    agenix.url = "github:ryantm/agenix";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
-
-    home.url = "github:nix-community/home-manager/release-22.05";
-    home.inputs.nixpkgs.follows = "nixpkgs";
-
-    deploy.url = "github:serokell/deploy-rs";
-    deploy.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Utilities for building our flake
-    flake-utils.url = "github:numtide/flake-utils";
+    deploy = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Convenience modules for hardware-specific quirks
     hardware.url = "github:nixos/nixos-hardware";
@@ -34,13 +37,14 @@
     nixos-generators.url = "github:nix-community/nixos-generators";
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
-    nvfetcher.url = "github:berberman/nvfetcher";
-    nvfetcher.inputs.nixpkgs.follows = "nixpkgs";
+    nvfetcher = {
+      url = "github:berberman/nvfetcher";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils, ... }@inputs:
     let
-      # Bring some functions into scope (from builtins and other flakes)
       # inherit (builtins) attrValues;
       inherit (flake-utils.lib) eachSystemMap defaultSystems;
       inherit (nixpkgs.lib) nixosSystem;
@@ -50,7 +54,6 @@
       # Home configurations
       # Accessible via 'home-manager'
       homeConfigurations = {
-        # FIXME: Replace with your username@hostname
         duck = homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
@@ -59,34 +62,22 @@
         };
       };
 
-      # System configurations
-      # Accessible via 'nixos-rebuild'
       nixosConfigurations = {
         nixos = nixosSystem {
           system = "x86_64-linux";
 
           modules = [
             home-manager.nixosModules.home-manager
-            # >> Main NixOS configuration file <<
             ./hosts/powerspecnixos/configuration.nix
-            # Adds your custom NixOS modules
-            # ./modules/nixos
-            # Adds overlays
-            # { nixpkgs.overlays = attrValues overlays; }
           ];
           # Make our inputs available to the config (for importing modules)
           specialArgs = { inherit inputs; };
         };
       };
 
-      # Packages
-      # Accessible via 'nix build'
       packages = eachDefaultSystemMap (system:
-        # Propagate nixpkgs' packages, with our overlays applied
         import nixpkgs { inherit system; });
 
-      # Devshell for bootstrapping
-      # Accessible via 'nix develop'
       devShells = eachDefaultSystemMap (system: {
         default = import ./shell.nix { pkgs = packages.${system}; };
       });
