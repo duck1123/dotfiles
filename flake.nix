6f5458744e03a3aff26f5f4300f4a98bfe722601
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-06-15 00:41:09 -0400

    chromium

--- flake.nix
+++ flake.nix
@@ -233,6 +233,20 @@
       inherit (flake-utils.lib) eachSystemMap defaultSystems;
       inherit (nixpkgs.lib) nixosSystem;
       inherit (home-manager.lib) homeManagerConfiguration;
+      widevine = nixpkgs.fetchurl {
+        url = "https://dl.google.com/widevine-cdm/4.10.2252.0-linux-x64.zip";
+        sha256 = "<sha256-hash-of-the-zip-file>";
+      };
+
+      widevine-unzip = nixpkgs.stdenv.mkDerivation {
+        name = "widevine-unzip";
+        src = widevine;
+        buildInputs = [ nixpkgs.unzip ];
+        buildPhase = ''
+          unzip $src -d $out
+        '';
+      };
+
       eachDefaultSystemMap = eachSystemMap defaultSystems;
       identities = rec {
         deck = {
@@ -327,6 +341,7 @@
           modules = [
             hyprpanel.homeManagerModules.hyprpanel
             stylix.homeModules.stylix
+            widevine-unzip
             ./hosts/powerspecnix/home-for-flake.nix
           ];
         };
@@ -362,6 +377,7 @@
             ./hosts/powerspecnix/configuration.nix
             disko.nixosModules.disko
             sops-nix.nixosModules.sops
+            widevine-unzip
           ];
           specialArgs = {
             inherit inputs;
