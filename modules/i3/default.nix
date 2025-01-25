68fb74efc5d716854b60be532f1f77e60f407a9b
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-01-25 00:30:58 -0500

    i3

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -1,6 +1,53 @@
-{ pkgs, ... }: {
-  services.xserver.windowManager.i3 = {
-    enable = true;
-    package = pkgs.i3-gaps;
+{ inputs, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    neovim
+    curl
+    cmatrix
+    neofetch
+    rofi
+    xfce.thunar
+    htop
+    git
+    xfce.ristretto
+    feh
+    lxappearance
+    imagemagick
+    zip
+    jq
+    unzip
+    qemu_kvm
+  ];
+
+  services = {
+    displayManager.sddm = {
+      enable = true;
+      theme = "ocean";
+      wayland.enable = true;
+    };
+    gvfs.enable = true;
+    picom = {
+      enable = true;
+      fade = true;
+      # vSync = true;
+      shadow = true;
+      fadeDelta = 4;
+      inactiveOpacity = 0.8;
+      activeOpacity = 1;
+      # backend = "glx";
+      settings = {
+        blur = {
+          #method = "dual_kawase";
+          #  background = true;
+          strength = 5;
+        };
+      };
+    };
+    tumbler.enable = true;
+    xserver.windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+    };
   };
 }
