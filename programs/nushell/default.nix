b5575ff3539f4ab7c66d70156d47e6444281c4e6
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-02-16 11:13:58 -0500

    Add support for external completers

--- programs/nushell/default.nix
+++ programs/nushell/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     carapace
+    fish
     nushell
     nu_scripts
     nufmt
@@ -36,9 +37,45 @@
 
       extraConfig = ''
         let carapace_completer = {|spans|
-          carapace $spans.0 nushell $spans | from json
+          carapace $spans.0 nushell $spans
+            | from json
+            | if ($in | default [] | where value == $"($spans | last)ERR" | is-empty) { $in } else { null }
         }
-      '';
+
+        let fish_completer = {|spans|
+          fish --command $'complete "--do-complete=($spans | str join " ")"'
+            | from tsv --flexible --noheaders --no-infer
+            | rename value description
+        }
+
+        # This completer will use carapace by default
+        let external_completer = {|spans|
+          let expanded_alias = scope aliases
+            | where name == $spans.0
+            | get -i 0.expansion
+
+          let spans = if $expanded_alias != null {
+              $spans
+                | skip 1
+                | prepend ($expanded_alias | split row ' ' | take 1)
+            } else {
+              $spans
+            }
+
+          match $spans.0 {
+            # carapace completions are incorrect for nu
+            nu => $fish_completer
+            # fish completes commits and branch names in a nicer way
+            git => $fish_completer
+            # carapace doesn't have completions for asdf
+            asdf => $fish_completer
+            # use zoxide completions for zoxide commands
+            _ => $carapace_completer
+          } | do $in $spans
+        }
+
+        $env.config.completions.external = { enable: true completer: $external_completer }
+        '';
       shellAliases = {
         d = "devspace";
         dr = "devspace run";
