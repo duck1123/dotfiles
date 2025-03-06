d7f28809df8125f2b60c8043149aab637fefdeca
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-06 16:41:21 -0500

    remove package comments

--- programs/developer/default.nix
+++ programs/developer/default.nix
@@ -3,44 +3,22 @@
     argo
     argocd
     # arkade
-
-    # Next generation multi-platform command line experience for Azure
     azure-cli
     azure-cli-extensions.powerbidedicated
-
-    # Universal SQL Client for developers, DBA and analysts. Supports MySQL, PostgreSQL, MariaDB, SQLite, and more
     dbeaver-bin
-
     devpod
     devpod-desktop
-
     devspace
-
-    # Domain name server
     dig
-
-    # Highly configurable DNS proxy for penetration testers and malware analysts
     # dnschef
-
-    # Simple command line utility to make DNS lookups to the specified server
     # dnslookup
-
-    # Scan for subdomains using brute-force techniques
     # dnsmap
-
-    # A command line tool for DigitalOcean services
     # doctl
-
     earthly
-
     # extraNodePackages.prettier
-
     # gnumake
-
     gitu
-
     go
-
     k3d
     k9s
     kchmviewer
@@ -48,38 +26,22 @@
     kubernetes-helm
     kubectl
     kustomize
-
-    # Kubernetes IDE
     # lens
-
     md-tangle
-
     # mr
-
     nixd
     nmap
-
     nodejs
-
     # openjdk
     # openjdk17
-
     # podman
     # podman-desktop
     # podman-tui
-
     # python3
-
     runme
-
     sqlite
-
-    # Command line tool for querying Sybase/MSSQL databases
     # sqsh
-
-    # Create fully functional virtual Kubernetes clusters
     # vcluster
-
     # virtualbox
   ];
 }
