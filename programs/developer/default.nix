16e5327a681ae61fe8198ead41492375fd72be7f
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-17 11:36:16 -0400

    Add databricks tools

--- programs/developer/default.nix
+++ programs/developer/default.nix
@@ -4,6 +4,9 @@
     argocd
     # arkade
     (azure-cli.withExtensions [ azure-cli-extensions.powerbidedicated ])
+    databricks-cli
+    databricks-sql-cli
+    dbx
     dbeaver-bin
     # devpod
     # devpod-desktop
