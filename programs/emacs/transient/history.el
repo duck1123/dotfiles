412ea63d495af98f04c024c3e9818afcb7d1d098
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-18 13:20:25 -0400

    Update magit history

--- programs/emacs/transient/history.el
+++ programs/emacs/transient/history.el
@@ -1,10 +1,17 @@
-((magit-blame
-  ("-w"))
- (magit-branch nil)
+((magit-branch nil)
+ (magit-cherry-pick
+  ("--ff")
+  nil)
  (magit-commit nil)
  (magit-fetch nil)
  (magit-gitignore nil)
  (magit-log
   ("-n256" "--graph" "--decorate")
   ("-n256" "--simplify-by-decoration" "--graph" "--decorate"))
- (magit-reset nil))
+ (magit-push nil
+             ("--force-with-lease"))
+ (magit-rebase
+  ("--autostash")
+  nil)
+ (magit-reset nil)
+ (magit-tag nil))
