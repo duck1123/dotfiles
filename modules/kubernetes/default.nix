3a7ef82078d3161fdd6c127fb1410411d36df24f
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-04-14 09:32:05 -0400

    add openiscsi  support

--- modules/kubernetes/default.nix
+++ modules/kubernetes/default.nix
@@ -1,6 +1,17 @@
 { config, inputs, pkgs, ... }: {
   environment.systemPackages = with pkgs; [ k3s nfs-utils openiscsi ];
 
+  # Create the expected symlink for iscsiadm
+  environment.etc."iscsiadm".source = "${pkgs.openiscsi}/bin/iscsiadm";
+  environment.etc."open-iscsi".source = "${pkgs.openiscsi}";
+  # Optionally, also symlink to /usr/bin if necessary
+  environment.sessionVariables.PATH = [ "/usr/bin" ];
+
+  system.activationScripts.makeUsrBinSymlinks = ''
+    mkdir -p /usr/bin
+    ln -sf ${pkgs.openiscsi}/bin/iscsiadm /usr/bin/iscsiadm
+  '';
+
   networking.firewall = {
     enable = false;
     allowedTCPPorts = [ 6443 ];
