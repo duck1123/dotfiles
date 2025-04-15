d7a8ebb2cc89ec1dc6fcb1c0f2f9813a1dfefbac
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-04-15 13:11:30 -0400

    format jujutsu

--- programs/jujutsu/default.nix
+++ programs/jujutsu/default.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }: {
-  home.packages = with pkgs; [
-    gg-jj
-    jj-fzf
-    jjui
-    jujutsu
-    lazyjj
-  ];
+  home.packages = with pkgs; [ gg-jj jj-fzf jjui jujutsu lazyjj ];
 
   programs.jujutsu = {
     enable = true;
