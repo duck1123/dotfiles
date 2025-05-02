f5fbd7294c1a9852cee0c6858a31d61b43ab983a
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-05-02 14:02:22 -0400

    restore gnome and plasma specialisations

--- machines/powerspecnix/configuration.nix
+++ machines/powerspecnix/configuration.nix
@@ -29,26 +29,26 @@ let
 in {
   imports = core ++ [ hyprland ];
 
-  # specialisation = {
-  #   budgie = {
-  #     inheritParentConfig = false;
-  #     configuration.imports = core ++ [ budgie ];
-  #   };
-  #   gnome = {
-  #     inheritParentConfig = false;
-  #     configuration.imports = core ++ [ gnome ];
-  #   };
-  #   i3 = {
-  #     inheritParentConfig = false;
-  #     configuration.imports = core ++ [ i3 ];
-  #   };
-  #   hyprland = {
-  #     inheritParentConfig = false;
-  #     configuration.imports = core ++ [ hyprland ];
-  #   };
-  #   plasma6 = {
-  #     inheritParentConfig = false;
-  #     configuration.imports = core ++ [ plasma6 ];
-  #   };
-  # };
+  specialisation = {
+    # budgie = {
+    #   inheritParentConfig = false;
+    #   configuration.imports = core ++ [ budgie ];
+    # };
+    gnome = {
+      inheritParentConfig = false;
+      configuration.imports = core ++ [ gnome ];
+    };
+    # i3 = {
+    #   inheritParentConfig = false;
+    #   configuration.imports = core ++ [ i3 ];
+    # };
+    # hyprland = {
+    #   inheritParentConfig = false;
+    #   configuration.imports = core ++ [ hyprland ];
+    # };
+    plasma6 = {
+      inheritParentConfig = false;
+      configuration.imports = core ++ [ plasma6 ];
+    };
+  };
 }
