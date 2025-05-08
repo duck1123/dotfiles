06310b272993efcd4b334078de1b78e0a8a63341
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-05-08 18:36:01 -0400

    syncthing

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -14,6 +14,13 @@ in {
 
     settings = {
       devices = {
+        "powespecnix" = {
+          id =
+            "JZHCKZ4-6WQOOMW-VK3J7WZ-LN7O3KU-C6IO3EY-3D4XBDT-P3R73MM-DUARSA3";
+          autoAcceptFolders = true;
+        };
+
+
         "Pixel 8" = {
           id =
             "7Y3NTUQ-MRUHGO4-5L34ZC7-EDRXHKA-QVCG7AJ-HWHIINY-OV5B2T7-OFQS2QP";
@@ -31,24 +38,30 @@ in {
             "TEED77K-QOLTQ37-BL76MFB-LJD46CW-EJ7CZTJ-7GQNEF6-FZAMQRP-BCCRTQ6";
           autoAcceptFolders = true;
         };
+
+        "inspernix" = {
+          id =
+            "OWMQLRL-CD5VB7H-A3T436E-6XT4H66-6XRF22Y-MQXMNAU-DFRNGOV-ADSKFAV";
+          autoAcceptFolders = true;
+        };
       };
 
       folders = {
         "Camera" = {
           label = "Camera";
           path = "/home/${username}/Camera";
-          devices = [ "Pixel 8" ];
+          devices = [ "Pixel 8" "powespecnix" "inspernix" ];
         };
 
         "keepass" = {
           label = "keepass";
           path = "/home/${username}/keepass";
-          devices = [ "Pixel 8" "VallenPC" ];
+          devices = [ "Pixel 8" "VallenPC" "inspernix" "powespecnix" ];
         };
 
         "org-roam" = {
           path = "/home/${username}/org-roam";
-          devices = [ "Pixel 8" ];
+          devices = [ "Pixel 8" "inspernix" "powespecnix" ];
           versioning = {
             type = "simple";
             params.keep = "10";
@@ -57,7 +70,7 @@ in {
 
         "steamdeck-renpy" = {
           path = "/home/${username}/.renpy";
-          devices = [ "steamdeck" ];
+          devices = [ "steamdeck" "powespecnix" ];
         };
       };
 
