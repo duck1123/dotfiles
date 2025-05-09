9503fae35d86463e2522f4ea48c611044b158dae
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-05-08 22:31:19 -0400

    add nix-bitcoin

--- flake.nix
+++ flake.nix
@@ -201,6 +201,15 @@
       url = "github:cachix/git-hooks.nix";
     };
 
+    nix-bitcoin = {
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-unstable.follows = "nixpkgs";
+      };
+      url = "github:fort-nix/nix-bitcoin/release";
+    };
+
     sops-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:Mic92/sops-nix";
@@ -235,8 +244,9 @@
     };
   };
 
-  outputs = { colmena, disko, flake-utils, home-manager, hyprpanel, nixpkgs
-    , nixos-facter-modules, sops-nix, stylix, zen-browser, ... }@inputs:
+  outputs = { colmena, disko, flake-utils, home-manager, hyprpanel, nix-bitcoin
+    , nixpkgs, nixos-facter-modules, sops-nix, stylix, zen-browser, ...
+    }@inputs:
     let
       inherit (flake-utils.lib) eachSystemMap defaultSystems;
       inherit (nixpkgs.lib) nixosSystem;
@@ -308,6 +318,7 @@
           };
           modules = [
             hyprpanel.homeManagerModules.hyprpanel
+            nix-bitcoin.nixosModules.default
             stylix.homeModules.stylix
             zen-browser.homeModules.beta
             ./hosts/vavirl-pw0bwnq8/home-for-flake.nix
@@ -322,6 +333,7 @@
           };
           modules = [
             hyprpanel.homeManagerModules.hyprpanel
+            nix-bitcoin.nixosModules.default
             stylix.homeModules.stylix
             zen-browser.homeModules.beta
             ./hosts/steamdeck/home-for-flake.nix
@@ -336,6 +348,7 @@
           };
           modules = [
             hyprpanel.homeManagerModules.hyprpanel
+            nix-bitcoin.nixosModules.default
             stylix.homeModules.stylix
             zen-browser.homeModules.beta
             ./hosts/powerspecnix/home-for-flake.nix
@@ -350,6 +363,7 @@
           };
           modules = [
             hyprpanel.homeManagerModules.hyprpanel
+            nix-bitcoin.nixosModules.default
             stylix.homeModules.stylix
             zen-browser.homeModules.beta
             ./hosts/inspernix/home-for-flake.nix
