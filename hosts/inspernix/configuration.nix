9bedd8636fd6a7b9b2727c72ba3d08084803f361
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-05-24 11:22:26 -0400

    Add dependencies for hyprpanel

--- hosts/inspernix/configuration.nix
+++ hosts/inspernix/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 let
   core = [
     ./hardware-configuration.nix
@@ -13,8 +13,18 @@ let
   ];
 in {
   imports = core ++ [ ../../modules/hyprland ];
+
+  environment.systemPackages = with pkgs; [
+    libgtop
+    wl-clipboard
+  ];
+
   hardware.bluetooth.enable = true;
-  services.upower.enable = true;
+
+  services = {
+    power-profiles-daemon.enable = true;
+    upower.enable = true;
+  };
 
   specialisation.gnome = {
     inheritParentConfig = false;
