9f75c7813148fabe7ad8be07f81d53ec3dc1d723
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-05-26 12:08:52 -0400

    fix budgie

--- modules/budgie/default.nix
+++ modules/budgie/default.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }: {
   services = {
-    xserver = {
-      desktopManager.budgie.enable = true;
-      displayManager.lightdm.enable = true;
-    };
     displayManager.defaultSession = "budgie-desktop";
+    xserver.desktopManager.budgie.enable = true;
   };
 }
