bc08c92fbf7fc6669e608276bb4817d55a923903
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-06-03 18:13:10 -0400

    formatter

--- flake.nix
+++ flake.nix
@@ -408,6 +408,14 @@
         };
       };
 
+      # formatter = forAllSystems (
+      #   system: let
+      #     pkgs = nixpkgs.legacyPackages.${system};
+      #   in
+      #     pkgs.alejandra
+      # );
+
+      
       # Home configurations
       # Accessible via 'home-manager'
       homeConfigurations =
