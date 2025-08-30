f1e54d7a317a69dde3f80c918e3219a21c017b3c
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-08-30 12:33:19 -0400

    Force platform theme for kde
    Fixes conflict with stylix

--- modules/plasma6/default.nix
+++ modules/plasma6/default.nix
@@ -1,1 +1,6 @@
-{ ... }: { services.desktopManager.plasma6.enable = true; }
+{ lib, ... }: {
+  services.desktopManager.plasma6.enable = true;
+
+  # Force override the Qt platform theme to use "kde" instead of "kde6"
+  qt.platformTheme = lib.mkForce "kde";
+}
