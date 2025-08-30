f44cb567e92e86222a99fe3a028caebb361dc7db
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-08-30 09:48:36 -0400

    assign hosts

--- modules/flakeModules/default.nix
+++ modules/flakeModules/default.nix
@@ -4,13 +4,19 @@ let
   hostSubmodule = types.submodule {
     options = {
       id = mkOption {
-        type = types.str;
+        type = types.int;
         description = "The id";
       };
     };
   };
 in {
   options = {
+    host = mkOption {
+      type = hostSubmodule;
+      description = "The host";
+      default = { };
+    };
+
     hosts = mkOption {
       type = types.attrsOf hostSubmodule;
       description = "Map of hosts with their configurations";
