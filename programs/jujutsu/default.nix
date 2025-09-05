f1617e266d106110c75f22b09c3f3d20d1261d9f
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-09-05 09:35:25 -0400

    less jj

--- programs/jujutsu/default.nix
+++ programs/jujutsu/default.nix
@@ -1,6 +1,10 @@
 { host, lib, pkgs, ... }: {
   config = lib.mkIf host.features.jujutsu.enable {
-    home.packages = with pkgs; [ gg-jj jj-fzf jjui jujutsu lazyjj ];
+    home.packages = with pkgs; [
+      # gg-jj jj-fzf
+      jjui jujutsu
+      # lazyjj
+    ];
 
     programs.jujutsu = {
       enable = true;
