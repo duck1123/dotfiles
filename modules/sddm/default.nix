b287937c5096235ebbf644f0278fa1916fc9fc71
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-09-13 12:23:05 -0400

    Change sddm theme

--- modules/sddm/default.nix
+++ modules/sddm/default.nix
@@ -1,5 +1,12 @@
 { host, lib, pkgs, ... }: {
   config = lib.mkIf host.features.sddm.enable {
+    environment.etc."sddm/themes/Breeze-Noir-Dark".source = pkgs.fetchFromGitHub {
+      owner = "L4ki";
+      repo = "Breeze-Noir-Dark";
+      rev = "master";
+      sha256 = "sha256-0GM3pmJ04hvoxuD7S0V7Wl/MP6kALiqTDWLdkLH6+/A=";
+    };
+
     environment.etc."sddm/themes/mybreeze".source = pkgs.fetchFromGitHub {
       owner = "L4ki";
       repo = "MyBreeze-Plasma-Themes";
@@ -10,7 +17,7 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      theme = "mybreeze";
+      theme = "Breeze-Noir-Dark";
       settings.Theme = {
         Font = "DejaVu Sans";
         FontSize = "14";
