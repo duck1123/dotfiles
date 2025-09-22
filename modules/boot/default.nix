5325e511a0601772543dbbd75742c1264948e9d6
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-09-22 19:24:26 -0400

    efi boot

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -1,16 +1,38 @@
-{ pkgs, lib, ... }: {
-  # Bootloader.
-  boot = {
-    binfmt.registrations.appimage = {
-      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-      magicOrExtension = "\\x7fELF....AI\\x02";
-      mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
-      offset = 0;
-      recognitionType = "magic";
-      wrapInterpreterInShell = false;
-    };
+{ pkgs, lib, config, ... }: {
+  options.boot.efiBoot = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Enable EFI boot configuration";
+  };
 
-    kernelModules = [ "dm_crypt" ];
-    plymouth.enable = true;
+  config = {
+    # Common boot configuration that applies to all systems
+    boot = {
+      binfmt.registrations.appimage = {
+        interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+        magicOrExtension = "\\x7fELF....AI\\x02";
+        mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+        offset = 0;
+        recognitionType = "magic";
+        wrapInterpreterInShell = false;
+      };
+
+      kernelModules = [ "dm_crypt" ];
+
+      plymouth.enable = true;
+    };
+  } // lib.mkIf config.boot.efiBoot {
+    boot.loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = lib.mkDefault false;
+      };
+    };
   };
 }
