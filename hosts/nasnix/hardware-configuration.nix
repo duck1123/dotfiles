cccbd02c0106a2bf5efd9fb31e4b28205da9c6d6
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-09-26 00:00:51 -0400

    Update bindings

--- hosts/nasnix/hardware-configuration.nix
+++ hosts/nasnix/hardware-configuration.nix
@@ -1,22 +1,49 @@
-{ lib, modulesPath, ... }: {
+{ lib, modulesPath, ... }:
+let nas-ip = "192.168.0.124";
+in {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/1f61c6bf-be89-48fe-8081-7a74ec707a38";
-    fsType = "ext4";
+    initrd = {
+      availableKernelModules =
+        [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+      kernelModules = [ ];
+    };
+
+    kernelModules = [ "kvm-intel" ];
   };
 
-  # Add NFS mount for Videos share
-  fileSystems."/mnt/videos" = {
-    device = "192.168.0.124:/volume1/Videos";
-    fsType = "nfs";
-    options = [ "nfsvers=3" "ro" "hard" "timeo=600" "retrans=2" "_netdev" ];
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/1f61c6bf-be89-48fe-8081-7a74ec707a38";
+      fsType = "ext4";
+    };
+
+    "/mnt/books" = {
+      device = "${nas-ip}:/volume1/Books";
+      fsType = "nfs";
+      options = [ "nfsvers=3" "ro" "hard" "timeo=600" "retrans=2" "_netdev" ];
+    };
+
+    "/mnt/downloads" = {
+      device = "${nas-ip}:/volume1/Downloads";
+      fsType = "nfs";
+      options = [ "nfsvers=3" "ro" "hard" "timeo=600" "retrans=2" "_netdev" ];
+    };
+
+    "/mnt/music" = {
+      device = "${nas-ip}:/volume1/Music";
+      fsType = "nfs";
+      options = [ "nfsvers=3" "ro" "hard" "timeo=600" "retrans=2" "_netdev" ];
+    };
+
+    "/mnt/videos" = {
+      device = "${nas-ip}:/volume1/Videos";
+      fsType = "nfs";
+      options = [ "nfsvers=3" "ro" "hard" "timeo=600" "retrans=2" "_netdev" ];
+    };
   };
 
   swapDevices = [ ];
