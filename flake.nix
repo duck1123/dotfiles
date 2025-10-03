6a26f327bac9e6fdec047227be3e71c899a65a39
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-10-02 20:52:23 -0400

    remove caches

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   nixConfig = {
     extra-experimental-features = "nix-command flakes";
-    extra-substituters =
-      [ "https://duck1123.cachix.org" "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "duck1123.cachix.org-1:Cj3r3BH7Xuy0zFWy8V/VIB3F7+Gi1m9HB302E9UGV3E="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    # extra-substituters =
+    #   [ "https://duck1123.cachix.org" "https://nix-community.cachix.org" ];
+    # extra-trusted-public-keys = [
+    #   "duck1123.cachix.org-1:Cj3r3BH7Xuy0zFWy8V/VIB3F7+Gi1m9HB302E9UGV3E="
+    #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    # ];
   };
 
   inputs = {
