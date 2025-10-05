953eca68bfa5036651650d720605126ed6adc8e0
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-10-05 08:29:21 -0400

    changes

--- modules/sddm/default.nix
+++ modules/sddm/default.nix
@@ -3,6 +3,8 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      package = pkgs.libsForQt5.sddm;
+      extraPackages = with pkgs; [ libsForQt5.qtgraphicaleffects ];
       sugarCandyNix = {
         enable = true;
         settings = {
