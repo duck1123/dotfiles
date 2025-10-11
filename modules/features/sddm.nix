0314d1ae065571865e434bab1c65407ee99b264e
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-10-11 19:06:02 -0400

    theme

--- modules/features/sddm.nix
+++ modules/features/sddm.nix
@@ -3,9 +3,13 @@
     config = lib.mkIf config.host.features.sddm.enable {
       services.displayManager.sddm = {
         enable = true;
-        wayland.enable = true;
         package = pkgs.kdePackages.sddm;
-        extraPackages = with pkgs; [ qt6.qtdeclarative qt6.qtsvg qt6.qt5compat ];
+
+        settings.Theme = {
+          Current = "sddm-sugar-candy-nix";
+          ThemeDir = "/run/current-system/sw/share/sddm/themes";
+        };
+
         sugarCandyNix = {
           enable = true;
           settings = {
@@ -16,6 +20,9 @@
             PartialBlur = true;
           };
         };
+
+        theme = lib.mkForce "sddm-sugar-candy-nix";
+        wayland.enable = true;
       };
     };
   };
