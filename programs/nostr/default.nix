bcc2847dcd2f3f3a3e9419506fb96f24e3df6b8b
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-10-11 09:17:27 -0400

    Remove package description comments

--- programs/nostr/default.nix
+++ programs/nostr/default.nix
@@ -1,16 +1,9 @@
 { config, lib, pkgs, ... }: {
   config = lib.mkIf config.host.features.nostr.enable {
     home.packages = with pkgs; [
-      # CLI application for nostr
       algia
-
-      # Desktop client for nostr, an open social media protocol
       # gossip
-
-      # Command-line tool for Nostr things
       nak
-
-      # TUI client for Nostr
       nostui
     ];
   };
