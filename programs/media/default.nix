a0c4fb80c1197a8692f905db60350144da00e964
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-10-27 23:28:55 -0400

    Move kodi support to media module

--- programs/media/default.nix
+++ programs/media/default.nix
@@ -1,11 +1,6 @@
 { host, lib, pkgs, ... }: {
   config = lib.mkIf host.features.media.enable {
-    home.packages = with pkgs; [
-      kodi
-      plex
-      # plex-media-player
-      vlc
-      # yt-dlp
-    ];
+    home.packages = with pkgs; [ plex vlc ];
+    programs.kodi.enable = false;
   };
 }
