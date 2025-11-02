bb18349d8955cee3ee2b2269f75207cdb5c81f1b
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-11-02 18:27:51 -0500

    Add options for home-manager modules

--- modules/flake/modules-option.nix
+++ modules/flake/modules-option.nix
@@ -1,14 +1,21 @@
 { lib, ... }: {
   options.flake.modules = lib.mkOption {
     type = lib.types.submodule {
-      options.nixos = lib.mkOption {
-        type = lib.types.attrsOf lib.types.anything;
-        description = "NixOS modules for different hosts";
-        default = { };
+      options = {
+        home-manager = lib.mkOption {
+          type = lib.types.attrsOf lib.types.anything;
+          description = "NixOS modules for different hosts";
+          default = { };
+        };
+
+        nixos = lib.mkOption {
+          type = lib.types.attrsOf lib.types.anything;
+          description = "NixOS modules for different hosts";
+          default = { };
+        };
       };
     };
     description = "Flake modules for different systems";
     default = { };
   };
 }
-
