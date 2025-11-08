731516ae10a83f5c5100772c54cd53b4e4a175d8
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-11-08 12:53:17 -0500

    no wsl

--- modules/flake/nixosConfigurations.nix
+++ modules/flake/nixosConfigurations.nix
@@ -1,11 +1,10 @@
 { inputs, ... }:
-let inherit (inputs.self.lib.mk-os) wsl linux;
+let inherit (inputs.self.lib.mk-os) linux;
 in {
   flake.nixosConfigurations = {
     inspernix = linux "inspernix";
     nasnix = linux "nasnix";
     powerspecnix = linux "powerspecnix";
     vidcentre = linux "vidcentre";
-    # vavirl-pw0bwnq8 = wsl "vavirl-pw0bwnq8";
   };
 }
