0ddbe8868827d477e5f5b0af6b5682683bf75bec
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-11-15 15:02:37 -0500

    flatten host config

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,10 +1,10 @@
 { config, ... }:
 let system = "x86_64-linux";
 in {
-  inspernix = import ./inspernix { inherit config system; };
-  nasnix = import ./nasnix { inherit config system; };
-  pixel8 = import ./pixel8 { inherit config system; };
-  powerspecnix = import ./powerspecnix { inherit config system; };
-  steamdeck = import ./steamdeck { inherit config system; };
-  vavirl-pw0bwnq8 = import ./vavirl-pw0bwnq8 { inherit config system; };
+  inspernix = import ./inspernix.nix { inherit config system; };
+  nasnix = import ./nasnix.nix { inherit config system; };
+  pixel8 = import ./pixel8.nix { inherit config system; };
+  powerspecnix = import ./powerspecnix.nix { inherit config system; };
+  steamdeck = import ./steamdeck.nix { inherit config system; };
+  vavirl-pw0bwnq8 = import ./vavirl-pw0bwnq8.nix { inherit config system; };
 }
