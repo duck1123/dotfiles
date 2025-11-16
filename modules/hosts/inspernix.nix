c9af611c14f8935fddd1a148368ec8882eabf63d
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-11-15 19:09:53 -0500

    unmask

--- modules/hosts/inspernix.nix
+++ modules/hosts/inspernix.nix
@@ -153,10 +153,6 @@ in
             "/boot" = {
               device = "/dev/disk/by-uuid/1D60-65FB";
               fsType = "vfat";
-              options = [
-                "fmask=0077"
-                "dmask=0077"
-              ];
             };
           }
           // lib.optionalAttrs mount-nas {
