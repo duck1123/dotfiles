c23ed621c46ba4bef9783502c61e3ed50bf513c2
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-01-16 07:43:49 -0500

    fix budgie rename

--- modules/environments/budgie.nix
+++ modules/environments/budgie.nix
@@ -1,8 +1,8 @@
 { ... }: {
   flake.modules.nixos.environments-budgie = { ... }: {
     services = {
+      desktopManager.budgie.enable = true;
       displayManager.defaultSession = "budgie-desktop";
-      xserver.desktopManager.budgie.enable = true;
     };
   };
 }
