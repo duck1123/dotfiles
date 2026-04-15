087c44a2fec242aa2f993c51be97b309cb34dfa9
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-04-15 19:14:47 -0400

    migrating master application to dotfiles

--- modules/kubernetes/default.nix
+++ modules/kubernetes/default.nix
@@ -3,15 +3,7 @@
   imports = [
     # Pull in all lib functions (loadSecrets, fromYAML, mkArgoApp, etc.),
     # the nixidyApps option, all application modules, and the ageRecipients module.
-    # Use path-based imports so this works without k3s-fleetops declaring a flakeModule.
-    "${inputs.k3s-fleetops}/modules/flake/flake-parts.nix"
-    "${inputs.k3s-fleetops}/modules/lib/fromYAML.nix"
-    "${inputs.k3s-fleetops}/modules/lib/toYAML.nix"
-    "${inputs.k3s-fleetops}/modules/lib/loadSecrets.nix"
-    "${inputs.k3s-fleetops}/modules/lib/mkArgoApp.nix"
-    "${inputs.k3s-fleetops}/modules/lib/waitForGluetun.nix"
-    "${inputs.k3s-fleetops}/modules/applications.nix"
-    "${inputs.k3s-fleetops}/modules/sops.nix"
+    inputs.k3s-fleetops.flakeModules.default
     ./nixidy-envs.nix
   ];
 }
