9e55b7403111be638244582ff9fa69efa61193bd
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-08-04 23:53:54 -0400

    disable nix csi

--- modules/kubernetes/_env/dev/nix-csi.nix
+++ modules/kubernetes/_env/dev/nix-csi.nix
@@ -1,4 +1,8 @@
 { ... }:
 {
-  services.nix-csi.enable = true;
+  # FIXME: nix-csi's own flake is currently broken when consumed as a flake
+  # input (pkgs/default.nix does `builtins.pathExists ../../pynixd`, which
+  # resolves outside the source tree and throws instead of returning false).
+  # Disabled until that's fixed upstream.
+  services.nix-csi.enable = false;
 }
