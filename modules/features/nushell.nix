3dc09ffd9774833db8560358066808750f447160
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-08-06 08:12:48 -0400

    refactor nushell config

--- modules/features/nushell.nix
+++ modules/features/nushell.nix
@@ -17,35 +17,39 @@
     }:
     {
       config = lib.mkIf config.host.features.nushell.enable {
-        home.packages = with pkgs; [
-          carapace
-          fish
-          jc
-          nushell
-          nu-lint
-          nu_scripts
-          nufmt
-          # nushellPlugins.highlight
-          nushellPlugins.formats
-          # nushellPlugins.polars: broken upstream, vendored ethnum 1.5.2 crate
-          # fails to compile under the current rustc (unsafe transmute size mismatch)
-          # nushellPlugins.polars
-          nushellPlugins.gstat
-          # nushellPlugins.units
-          nushellPlugins.query
-          # nushellPlugins.dbus
-          nushellPlugins.skim
-          # nushellPlugins.net
-          inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.nur-taskrunner
-          inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.pnu
-        ];
+        home = {
+          file = {
+            "nushell/completions".source = ../../nushell/completions;
+            "nushell/me.nu".source = builtins.fetchurl {
+              url = "https://raw.githubusercontent.com/ClipplerBlood/me.nu/09e4ee7fbee6a26cb8dd3041e9da1f4de2c8d119/me.nu";
+              sha256 = "sha256:1l3mhcwl2mvkz9qg3yzgz7xdrkdr4xzxfxj3mhv22knbaca5dacr";
+            };
+            "nushell/modules".source = ../../nushell/modules;
+          };
 
-        home.file."nushell/completions".source = ../../nushell/completions;
-        home.file."nushell/modules".source = ../../nushell/modules;
+          packages = with pkgs; [
+            carapace
+            fish
+            jc
+            nushell
+            nu-lint
+            nu_scripts
+            nufmt
+            # nushellPlugins.highlight
+            nushellPlugins.formats
+            # nushellPlugins.polars: broken upstream, vendored ethnum 1.5.2 crate
+            # fails to compile under the current rustc (unsafe transmute size mismatch)
+            # nushellPlugins.polars
+            nushellPlugins.gstat
+            # nushellPlugins.units
+            nushellPlugins.query
+            # nushellPlugins.dbus
+            nushellPlugins.skim
+            # nushellPlugins.net
+            inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.nur-taskrunner
+            inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.pnu
+          ];
 
-        home.file."nushell/me.nu".source = builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/ClipplerBlood/me.nu/09e4ee7fbee6a26cb8dd3041e9da1f4de2c8d119/me.nu";
-          sha256 = "sha256:1l3mhcwl2mvkz9qg3yzgz7xdrkdr4xzxfxj3mhv22knbaca5dacr";
         };
 
         programs = {
@@ -65,12 +69,8 @@
             '';
 
             shellAliases = {
-              d = "devspace";
-              dr = "devspace run";
               cat = "bat";
               hh = "hstr";
-              # psgrep = "ps -ef | grep -v grep | grep ";
-              rmr = "runme run";
             };
           };
         };
