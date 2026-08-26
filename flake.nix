ab6df69aec503b3fa497523b21432497dff69b9f
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-08-26 19:52:44 -0400

    update

--- flake.nix
+++ flake.nix
@@ -16,27 +16,13 @@
 
   inputs = {
     attic = {
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:zhaofengli/attic";
     };
 
-    clj-nix = {
-      inputs = {
-        devshell.follows = "devshell";
-        nix-fetcher-data.follows = "nix-fetcher-data";
-        nixpkgs.follows = "nixpkgs";
-      };
-      url = "github:jlesquembre/clj-nix";
-    };
-
-    devshell = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:numtide/devshell";
-    };
-
     flake-compat.url = "github:edolstra/flake-compat";
-
-    flake-file.url = "github:vic/flake-file";
 
     flake-parts = {
       inputs.nixpkgs-lib.follows = "nixpkgs-lib";
@@ -46,11 +32,6 @@
     flake-utils = {
       inputs.systems.follows = "systems";
       url = "github:numtide/flake-utils";
-    };
-
-    haumea = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/haumea";
     };
 
     home-manager = {
@@ -72,48 +53,22 @@
     k3s-fleetops = {
       inputs = {
         attic.follows = "attic";
-        clj-nix.follows = "clj-nix";
+        flake-compat.follows = "flake-compat";
         flake-parts.follows = "flake-parts";
         flake-utils.follows = "flake-utils";
         import-tree.follows = "import-tree";
-        make-shell.follows = "make-shell";
         nix-csi.follows = "nix-csi";
-        nix-fetcher-data.follows = "nix-fetcher-data";
         nix-kube-generators.follows = "nix-kube-generators";
         nixidy.follows = "nixidy";
         nixhelm.follows = "nixhelm";
         nixpkgs.follows = "nixpkgs";
-        nixpkgs-lib.follows = "nixpkgs-lib";
-        sops-nix.follows = "sops-nix";
-        systems.follows = "systems";
       };
       url = "github:duck1123/k3s-fleetops";
     };
 
-    kubenix = {
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-      url = "github:hall/kubenix";
-    };
-
-    make-shell = {
-      inputs.flake-compat.follows = "flake-compat";
-      url = "github:nicknovitski/make-shell";
-    };
-
-    nix-fetcher-data = {
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-      };
-      url = "github:jlesquembre/nix-fetcher-data";
-    };
-
     nix-csi = {
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.treefmt-nix.follows = "treefmt-nix";
       url = "github:Lillecarl/nix-csi";
     };
 
@@ -122,7 +77,6 @@
     nixhelm = {
       inputs = {
         flake-utils.follows = "flake-utils";
-        haumea.follows = "haumea";
         nix-kube-generators.follows = "nix-kube-generators";
         nixpkgs.follows = "nixpkgs";
       };
@@ -137,8 +91,6 @@
       };
       url = "github:arnarg/nixidy";
     };
-
-    nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
 
     nixos-wsl = {
       inputs = {
@@ -200,6 +152,13 @@
     };
 
     systems.url = "github:nix-systems/default";
+
+    treefmt-nix = {
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+      url = "github:numtide/treefmt-nix";
+    };
 
     zen-browser = {
       inputs = {
